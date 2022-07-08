d5d0ba6ba9091ff5857d0eaead2816a893127139
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-07-08 19:17:49 +0530

    basic slider introduced

--- src/components/MITable.js
+++ src/components/MITable.js
@@ -12,6 +12,8 @@ import { keys, values } from '@laufire/utils/lib';
 import { map } from '@laufire/utils/collection';
 import consolidatedData from '../services/consolidatedData';
 import { unique } from '@laufire/utils/predicates';
+import { Slider } from '@mui/material';
+import { Box } from '@mui/system';
 
 const MarkSheetD = (context) => {
 	const newData = consolidatedData(context);
@@ -19,40 +21,43 @@ const MarkSheetD = (context) => {
 		.filter(unique);
 
 	return (
-		<TableContainer
-			sx={ {
-				'width': 650,
-				'backgroundColor': 'lightBlue',
-				'& :hover': {
-					backgroundColor: 'yellow',
-				},
-			} }
-			component={ Paper }
-		>
-			<Table>
-				<TableHead>
-					<TableRow component="th">
-						{map(columns, (sub) =>
-							<TableCell
-								key={ sub }
-								component="th"
-								align="center"
-							>
-								{sub}</TableCell>)}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{map(newData, (row, i) => <TableRow key={ i }>{
-						map(values(row), (ele, j) =>
-							<TableCell
-								key={ j }
-								align="center"
-							>{ele} </TableCell>)
-					}
-					</TableRow>)}
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<Box sx={ { width: 100 } }>
+			<Slider defaultValue={ 50 } valueLabelDisplay="auto"/>
+			<TableContainer
+				sx={ {
+					'width': 650,
+					'backgroundColor': 'lightBlue',
+					'& :hover': {
+						backgroundColor: 'yellow',
+					},
+				} }
+				component={ Paper }
+			>
+				<Table>
+					<TableHead>
+						<TableRow component="th">
+							{map(columns, (sub) =>
+								<TableCell
+									key={ sub }
+									component="th"
+									align="center"
+								>
+									{sub}</TableCell>)}
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{map(newData, (row, i) => <TableRow key={ i }>{
+							map(values(row), (ele, j) =>
+								<TableCell
+									key={ j }
+									align="center"
+								>{ele} </TableCell>)
+						}
+						</TableRow>)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Box>
 	);
 };
 
