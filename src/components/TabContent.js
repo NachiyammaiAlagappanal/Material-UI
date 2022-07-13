148580b37f63ae1820a6dd7a9ae36b9f6a5f3112
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-07-13 11:32:03 +0530

    corrected bug in previous commit.

--- src/components/TabContent.js
+++ src/components/TabContent.js
@@ -1,11 +1,8 @@
-/* eslint-disable max-lines-per-function */
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import MarkSheetD from './MITable';
-
-// import MarkSheetD from './MITable';
 
 const TabPanel = (props) => {
 	const { value, index, children } = props;
@@ -19,8 +16,8 @@ const TabContent = (context) => {
 	return	<Box sx={ { backgroundColor: 'background.paper' } }>
 		<Tabs
 			value={ value }
-			onChange={ () => context.actions
-				.changingTab(1) }
+			onChange={ (event, data) => context.actions
+				.changingTab(data) }
 			textColor="inherit"
 			indicatorColor="secondary"
 		>
