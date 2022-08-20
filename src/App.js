2d8fb9fdb0879d8d2e39b2b19067006c00ba26aa
nvn-laufire <nvn@laufire.in>
2022-08-20 11:10:26 +0530

    image used and a link usage added.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { React } from 'react';
 import './App.scss';
 // import MarkSheetD from './components/MITable';
 import TabContent from './components/TabContent';
+import Image from './images/redMark.png';
 
 const App = (context) =>
 	<div className="App" role="App">
 		{TabContent(context)}
+		<a href="https://www.w3schools.com">
+			<img src={ Image } width="50" height="50"/></a>
 	</div>;
 
 export default App;
